cb422ddb3ca614650f1bc8fc28d2ff86b527890c
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2022-10-31 01:40:08 -0400

    more LC problems

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,10 +1,9 @@
 public class BinarySearch {
 
     public static int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
+        int low = 0, high = nums.length - 1;
 
-        while (high >= low) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] == target) {
                 return mid;
@@ -12,9 +11,7 @@ public class BinarySearch {
                 low = mid + 1;
             } else high = mid - 1;
         }
-
         return -1;
-
     }
 
     public static void main(String[] args) {
--- MoveZeroes.java
+++ MoveZeroes.java
@@ -3,25 +3,22 @@ import java.util.Arrays;
 public class MoveZeroes {
 
     public static void moveZeroes(int[] nums) {
-        int l = 0, r = 1;
-        while (r < nums.length - 1) {
-            if (nums[l] != nums[r] && (nums[l] == 0 || nums[r] == 0)) {
-                int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
-                l++;
-                r++;
-            } else if (nums[l] == nums[r]) {
-                l++;
-                r++;
+        int i = 0, j = 0;
+        while (i < nums.length) {
+            if (nums[i] != 0) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+                j++;
             }
+            i++;
         }
     }
 
     public static void main(String[] args) {
         int[] arr1 = new int[]{0, 1, 0, 3, 1};
         int[] arr2 = new int[]{0, 1, 0, 0, 9};
-        int[] arr3 = new int[]{0, 0};
+        int[] arr3 = new int[]{0, 1, 0};
 
         moveZeroes(arr1);
         System.out.println(Arrays.toString(arr1));
